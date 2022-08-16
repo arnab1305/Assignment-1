4ce247dec0d0794b4931178a7a5e016e642d90c5
ARNAB RANJAN DAS <darnab@pkrmsezdc.com>
2022-08-16 16:48:26 +0530

    first commit

---
+++ script/store.js
@@ -0,0 +1,58 @@
+var data = [
+  {
+    category: "News Videos",
+    name: "India News",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVzV2rCOyG7lOPs7XPxQ7kAUs-CigAI7LkCw&usqp=CAU",
+  },
+  {
+    category: "News Videos",
+    name: "China News",
+    image:
+      "https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA=",
+  },
+  {
+    category: "News Videos",
+    name: "USA News",
+    image:
+      "https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA=",
+  },
+  {
+    category: "Songs",
+    name: "Hindi Song",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSYVAMabEAzGvvl2MAMIubmmqxNBqdLu35Xg&usqp=CAU",
+  },
+  {
+    category: "Songs",
+    name: "English Song",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBgnPL5QK2zKsoy0hsKOOlwSQz1Sc3mXxWaQ&usqp=CAU",
+  },
+  {
+    category: "Songs",
+    name: "Telugu Song",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStz4YhDJwISi4IbUsBK2NR5C1SpVh3iziPuw&usqp=CAU",
+  },
+  {
+    category: "Animals",
+    name: "Dog",
+    image:
+      "https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg",
+  },
+  {
+    category: "Animals",
+    name: "Cat",
+    image:
+      "https://media.istockphoto.com/photos/cat-with-blue-eyes-looks-at-camera-picture-id1067347086?k=20&m=1067347086&s=612x612&w=0&h=Wxch207ChCoqnlqa5zvuy17J_YyApm42L6rUN3hml54=",
+  },
+  {
+    category: "Animals",
+    name: "Panda",
+    image:
+      "https://cdn.britannica.com/80/150980-050-84B9202C/Giant-panda-cub-branch.jpg",
+  },
+];
+sessionStorage.setItem("displayData", JSON.stringify(data));
+sessionStorage.setItem("copyDisplayData", JSON.stringify(data));
